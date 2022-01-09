e181cd4a5b38f2cd7dfe27a3746f49df185a9450
Aleks-Kar <Nomas86@yandex.ru>
2022-01-09 14:47:48 +0300

    daily update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 document.body.addEventListener('keydown', function(event) {
-  console.log(event.code);
   try {
     const btnDn = document.querySelector(`#${event.code.toLowerCase()}`);
-    // btnDn.style.boxShadow = '0 0 20px 10px rgb(255, 0, 0, 0.5), inset 0 0 20px rgb(255, 0, 0, 0.5)';
-    btnDn.className = 'button2';
-    document.body.addEventListener('keyup', function(event) {
-      const btnUp = document.querySelector(`#${event.code.toLowerCase()}`);
-      if (btnDn === btnUp) setTimeout(() => {
-        btnUp.className = 'button';
-      }, 100);
-    });
+    // console.log(event.code);
+    if (event.code === 'Tab' || event.code === 'OSLeft' || event.code === 'ContextMenu' || event.altKey || event.ctrlKey) {
+      console.log('first condition');
+      event.preventDefault();
+    } else {
+      btnDn.className = 'button-dn';
+      document.body.addEventListener('keyup', function(event) {
+        const btnUp = document.querySelector(`#${event.code.toLowerCase()}`);
+        if (btnDn === btnUp) setTimeout(function() {
+          btnUp.className = 'button-up';
+        }, 100);
+      });
+    }
   } catch (error) {
     // console.error(`${error.name}: ${error.message}`);
   }
