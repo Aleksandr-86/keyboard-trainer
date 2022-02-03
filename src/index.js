f31a2e2d1018556be7f51df788f474d1d3608c2a
Aleks-Kar <Nomas86@yandex.ru>
2022-02-03 18:41:06 +0300

    add: style for menu

--- src/index.js
+++ src/index.js
@@ -7,23 +7,24 @@ import {getBrowser} from "/src/scripts/browser-detector.js";
 const btnGenFromBuffer = document.querySelector('#btn2');
 
 // btnGenFromSite.addEventListener('click', function() {
-  // dataCall();
+// dataCall();
 // });
 
-  const str = '”””the ”hospital”. For the patient, the hospital is — “realist” only while he stays there. His greatest desire is to go back to the “nonhospital” world as fast as possible.\n' +
-    'What ”happens” inside any — organization is — effort and cost. To speak of “profit centers” in a business as we are wont to do is polite euphemism. There are only effort centers. The less an organization has to do to produce results, the better does its job. \n'
+const str = '”””the ”hospital”. For the patient, the hospital is — “realist” only while he stays there. His greatest desire is to go back to the “nonhospital” world as fast as possible.\n' +
+  'What ”happens” inside any — organization is — effort and cost. To speak of “profit centers” in a business as we are wont to do is polite euphemism. There are only effort centers. The less an organization has to do to produce results, the better does its job. \n'
 
 charInserter(strPreparer(str), 0);
-btnGenFromBuffer.addEventListener('click', async function() {
-  // defining browser
-  const br = getBrowser().browser;
-  if (br === 'chrome' || br === 'yabrowser') {
-    let str = await navigator.clipboard.readText();
-    charInserter(strPreparer(str), 0);
-  } else if (br === 'firefox') {
-    let str = document.querySelector('#input').value;
-    charInserter(strPreparer(str), 0);
-  }
-});
+
+// btnGenFromBuffer.addEventListener('click', async function() {
+//   // defining browser
+//   const br = getBrowser().browser;
+//   if (br === 'chrome' || br === 'yabrowser') {
+//     let str = await navigator.clipboard.readText();
+//     charInserter(strPreparer(str), 0);
+//   } else if (br === 'firefox') {
+//     let str = document.querySelector('#input').value;
+//     charInserter(strPreparer(str), 0);
+//   }
+// });
 
 document.addEventListener('keydown', event => keyboard(event));
