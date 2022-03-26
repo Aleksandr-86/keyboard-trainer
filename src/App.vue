862e362ecefe21e2919a3df0dbdaaf021c56740d
Aleks-Kar <Nomas86@yandex.ru>
2022-03-26 23:34:44 +0300

    vue variant

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <HelloWorld />
+</template>
+
+<style>
+#app {
+  width: 100vw;
+  height: 100vh;
+  background-color: aquamarine;
+
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: hsl(300, 100%, 20%);
+  /* margin-top: 60px; */
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+const newItem = ref('')
+
+function increment() {
+  count.value++
+}
+</script>
+
+<template>
+  <input v-model="newItem" placeholder="введите число" />
+  <p class="counter">{{ `${newItem.length}/200` }}</p>
+  <button class="btn" @click="increment">{{ count }}</button>
+</template>
+
+<style>
+input {
+  margin-top: 20px;
+  width: 700px;
+  height: 50px;
+  font-size: 40px;
+}
+
+.counter {
+  font-size: 40px;
+  color: hsl(120, 100%, 30%);
+}
+
+.btn {
+  margin: 100px 0 0 0;
+  width: 100px;
+  height: 100px;
+  font-size: 50px;
+}
+</style>
