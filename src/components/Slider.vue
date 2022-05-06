ee6292496fc741dd29e8f9acf364bafb4b7c8a21
Aleks-Kar <Nomas86@yandex.ru>
2022-05-07 01:29:09 +0300

    change: Slider.vue style and template

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, reactive } from 'vue'
+import { onMounted, reactive, computed } from 'vue'
 
 const hsla = reactive({
   hue: 0,
@@ -7,72 +7,76 @@ const hsla = reactive({
   lightness: 0,
   alpha: 0
 })
-// const output = document.querySelector('#demo')
 
-// console.warn(vol)
-// console.warn(o)
-// output.innerHTML = slider.value
-
-// slider.oninput = function () {
-//   output.innerHTML = this.value
-// }
-onMounted(() => {
-  const hue = document.body.querySelector('#hue')
-})
-
-const fn = function () {
-  hsla.hue = hue.valueAsNumber
-  console.warn(hsla.hue)
+const inputHue = function () {
+  hsla.hue = sliderhue.valueAsNumber
 }
+
+onMounted(() => {
+  hsla.hue = 355
+})
 </script>
 
 <template>
-  <label for="hue">{{ hsla.hue }}</label>
   <div class="slider-container">
-    <input
-      @click="fn"
-      type="range"
-      class="slider"
-      id="hue"
-      min="0"
-      max="360"
-      step="1"
-      value="20" />
+    <div class="slider-container-description">Левый указательный палец</div>
+    <div class="slider-description">Оттенок</div>
+    <div class="slider">
+      <input
+        @input="inputHue"
+        type="range"
+        class="slider-input"
+        id="sliderhue"
+        min="0"
+        max="360" />
+
+      <label class="slider-label" for="hue">{{ hsla.hue }}</label>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.slider-description {
+  text-align: left;
+}
+
 .slider-container {
   width: 380px;
   background-color: grey;
   margin-top: 10px;
 }
 
+.slider-container-description {
+  text-align: left;
+  margin-bottom: 5px;
+  background-color: red;
+}
+
 .slider {
+  display: flex;
+  /* float: left; */
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.slider-input {
   -webkit-appearance: none;
   /* width: 100%; */
-  width: 300px;
-  margin-right: 35px;
-  height: 15px;
+  width: 330px;
+  height: 12px;
   border-radius: 5px;
-  background: #d3d3d3;
+  background: hsl(0, 0%, 83%);
   outline: none;
   opacity: 0.7;
   -webkit-transition: 0.2s;
   transition: opacity 0.2s;
 }
 
-.slider:hover {
+.slider-input:hover {
   opacity: 1;
 }
 
-label {
-  /* margin-left: 15px; */
-  color: black;
-  width: 45px;
-}
-
-.slider::-webkit-slider-thumb {
+.slider-input::-webkit-slider-thumb {
   -webkit-appearance: none;
   appearance: none;
   width: 25px;
@@ -82,11 +86,17 @@ label {
   cursor: pointer;
 }
 
-.slider::-moz-range-thumb {
+.slider-input::-moz-range-thumb {
   width: 25px;
   height: 25px;
   border-radius: 50%;
   background: #4caf50;
   cursor: pointer;
+}
+
+.slider-label {
+  margin-left: 5px;
+  color: black;
+  width: 45px;
 }
 </style>
