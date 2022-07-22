deb39c5ee8b6fe1d06b024cbee12bfc367749553
Aleks-Kar <Nomas86@yandex.ru>
2022-07-22 22:42:03 +0300

    chore: change background list content

--- src/services/background-list.js
+++ src/services/background-list.js
@@ -2,50 +2,50 @@ export const arrBackgrounds = [
   {
     num: 1,
     name: 'bird',
-    author: 'Илья Бунин',
+    author: 'Егор Камелев',
     location: '',
-    link: 'https://vk.com/bunin_photo_sochi'
+    link: 'https://vk.com/ekamelev'
   },
   {
     num: 2,
-    name: 'krasnaya-polyana',
-    author: 'Илья Бунин',
-    location: 'Красная Поляна, Краснодарский край',
-    link: 'https://vk.com/bunin_photo_sochi'
-  },
-  {
-    num: 3,
-    name: 'kamchatka',
-    author: 'Александр Максин',
-    location: 'Камчатский край',
-    link: 'https://35photo.pro/maksin/'
-  },
-  {
-    num: 4,
-    name: 'altai-mountains',
-    author: 'Денис Лобанов',
-    location: 'Республика Алтай',
-    link: ''
-  },
-  {
-    num: 5,
-    name: 'dombay',
-    author: 'Илья Бунин',
-    location: 'Горная территория Домбай, Карачаево-Черкесская Республика',
-    link: 'https://vk.com/bunin_photo_sochi'
-  },
-  {
-    num: 6,
     name: 'bug',
     author: 'Егор Камелев',
     location: '',
     link: 'https://vk.com/ekamelev'
   },
   {
-    num: 7,
+    num: 3,
+    name: 'krasnaya-polyana',
+    author: 'Илья Бунин',
+    location: 'Красная Поляна, Краснодарский край',
+    link: 'https://vk.com/bunin_photo_sochi'
+  },
+  {
+    num: 4,
+    name: 'dombay',
+    author: 'Илья Бунин',
+    location: 'Горная территория Домбай, Карачаево-Черкесская Республика',
+    link: 'https://vk.com/bunin_photo_sochi'
+  },
+  {
+    num: 5,
+    name: 'altai-mountains',
+    author: 'Денис Лобанов',
+    location: 'Республика Алтай',
+    link: ''
+  },
+  {
+    num: 6,
     name: 'sea-lion',
     author: 'Игорь Дудковский',
     location: 'г. Невельск, Сахалинская область',
     link: 'https://35photo.pro/710081'
+  },
+  {
+    num: 7,
+    name: 'kamchatka',
+    author: 'Александр Максин',
+    location: 'Камчатский край',
+    link: 'https://35photo.pro/maksin/'
   }
 ]
