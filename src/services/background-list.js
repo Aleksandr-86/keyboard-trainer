6b8a22b64b69cc0d28fbabd6aa147635f9671c69
Aleks-Kar <Nomas86@yandex.ru>
2022-07-22 15:15:12 +0300

    refactor: changed the order of previews in settings bar

--- src/services/background-list.js
+++ src/services/background-list.js
@@ -1,45 +1,45 @@
 export const arrBackgrounds = [
   {
     num: 1,
+    name: 'bird',
+    author: 'Илья Бунин',
+    location: '',
+    link: 'https://vk.com/bunin_photo_sochi'
+  },
+  {
+    num: 2,
     name: 'krasnaya-polyana',
     author: 'Илья Бунин',
     location: 'Красная Поляна, Краснодарский край',
     link: 'https://vk.com/bunin_photo_sochi'
   },
   {
-    num: 2,
+    num: 3,
     name: 'kamchatka',
     author: 'Александр Максин',
     location: 'Камчатский край',
     link: 'https://35photo.pro/maksin/'
   },
   {
-    num: 3,
+    num: 4,
     name: 'altai-mountains',
     author: 'Денис Лобанов',
     location: 'Республика Алтай',
     link: ''
   },
   {
-    num: 4,
+    num: 5,
     name: 'dombay',
     author: 'Илья Бунин',
     location: 'Горная территория Домбай, Карачаево-Черкесская Республика',
     link: 'https://vk.com/bunin_photo_sochi'
   },
   {
-    num: 5,
+    num: 6,
     name: 'bug',
     author: 'Егор Камелев',
     location: '',
     link: 'https://vk.com/ekamelev'
-  },
-  {
-    num: 6,
-    name: 'bird',
-    author: 'Илья Бунин',
-    location: '',
-    link: 'https://vk.com/bunin_photo_sochi'
   },
   {
     num: 7,
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-process.env.BROWSER = "chrome";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/keyboard-trainer/",
   plugins: [vue()],
-  server: {
-    open: "index.html",
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
