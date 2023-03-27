e0eb99a849c175ff7d36b64d1e5b61d6c3ed313e
Александр Карпенко <96790009+Aleksandr-86@users.noreply.github.com>
2023-03-27 11:45:51 +0300

    chore: cleaning

--- src/components/TheField.vue
+++ src/components/TheField.vue
@@ -1,159 +1,154 @@
 <script setup>
-import { computed, onMounted, onUnmounted, reactive } from 'vue'
-import { useRouter } from 'vue-router'
-import { data, moveCaret, recordingStatistics } from '@/store/data.js'
-import { state } from '@/store/state.js'
-import { storage } from '@/store/storage.js'
-import click from '@/assets/sounds/click.mp3'
-import {
-  charTest,
-  isAuxiliaryKeys,
-  msToMinutes,
-  playAudio
-} from '@/services/helpers.js'
-import TheKeyboard from '@/components/TheKeyboard.vue'
-import TheCurrentStats from './TheCurrentStats.vue'
+import { computed, onMounted, onUnmounted, reactive } from "vue";
+import { useRouter } from "vue-router";
+import { data, moveCaret, recordingStatistics } from "@/store/data.js";
+import { state } from "@/store/state.js";
+import { storage } from "@/store/storage.js";
+import click from "@/assets/sounds/click.mp3";
+import { charTest, isAuxiliaryKeys, msToMinutes, playAudio } from "@/services/helpers.js";
+import TheKeyboard from "@/components/TheKeyboard.vue";
+import TheCurrentStats from "./TheCurrentStats.vue";
 
 const events = reactive({
   keyDn: Object,
   keyValue: {},
-  capsLock: false
-})
+  capsLock: false,
+});
 
-const router = useRouter()
+const router = useRouter();
 
-const statArr = computed(() => data.statArr)
-const indexArr = computed(() => data.indexArr)
-const firstIndex = computed(() => data.firstIndex)
+const statArr = computed(() => data.statArr);
+const indexArr = computed(() => data.indexArr);
+const firstIndex = computed(() => data.firstIndex);
 
 // переменные цвета
-const fieldBackground = computed(() => storage.field.background)
-const charBackground = computed(() => storage.field.charBackground)
-const charColor = computed(() => storage.field.charColor)
-const charCorrectColor = computed(() => storage.field.charCorrectColor)
-const charWrongColor = computed(() => storage.field.charWrongColor)
-const charRevisedColor = computed(() => storage.field.charRevisedColor)
-const charSpecialColor = computed(() => storage.field.charSpecialColor)
-const caretBackground = computed(() => storage.field.caretBackground)
+const fieldBackground = computed(() => storage.field.background);
+const charBackground = computed(() => storage.field.charBackground);
+const charColor = computed(() => storage.field.charColor);
+const charCorrectColor = computed(() => storage.field.charCorrectColor);
+const charWrongColor = computed(() => storage.field.charWrongColor);
+const charRevisedColor = computed(() => storage.field.charRevisedColor);
+const charSpecialColor = computed(() => storage.field.charSpecialColor);
+const caretBackground = computed(() => storage.field.caretBackground);
 
 // переменные эффекта отбрасываемой тени
 const charCorrectShadow = computed(() => {
   if (storage.shadow.charCorrect) {
-    return 'drop-shadow(3px 2px 2px)'
+    return "drop-shadow(3px 2px 2px)";
   } else {
-    return 'none'
+    return "none";
   }
-})
+});
 
 const charWrongShadow = computed(() => {
   if (storage.shadow.charWrong) {
-    return 'drop-shadow(3px 2px 2px)'
+    return "drop-shadow(3px 2px 2px)";
   } else {
-    return 'none'
+    return "none";
   }
-})
+});
 
 const charRevisedShadow = computed(() => {
   if (storage.shadow.charRevised) {
-    return 'drop-shadow(3px 2px 2px)'
+    return "drop-shadow(3px 2px 2px)";
   } else {
-    return 'none'
+    return "none";
   }
-})
+});
 
 const charSpecialShadow = computed(() => {
   if (storage.shadow.charSpecial) {
-    return 'drop-shadow(3px 2px 2px)'
+    return "drop-shadow(3px 2px 2px)";
   } else {
-    return 'none'
+    return "none";
   }
-})
+});
 
 // возвращает символы поля для набора
 const charsArr = computed(() =>
   data.fragmentArr.slice(data.firstIndex, data.firstIndex + 200)
-)
+);
 
 // обработка события ввода символа с клавиатуры
 const eListener = function (e) {
-  events.keyDn = e
-  events.keyValue = e.key
-  const code = e.code
+  events.keyDn = e;
+  events.keyValue = e.key;
+  const code = e.code;
 
   if (
-    (data.indexArr === 0 && code === 'Enter') ||
-    (data.indexArr === 0 && code === 'Backspace')
+    (data.indexArr === 0 && code === "Enter") ||
+    (data.indexArr === 0 && code === "Backspace")
   ) {
-    return
-  } else if (data.indexArr !== 0 && code === 'Enter') {
-    data.timerStop = performance.now()
-    router.push({ name: 'stats' })
-    return
+    return;
+  } else if (data.indexArr !== 0 && code === "Enter") {
+    data.timerStop = performance.now();
+    router.push({ name: "stats" });
+    return;
   }
 
   if (isAuxiliaryKeys(code)) {
-    e.preventDefault()
-    return
+    e.preventDefault();
+    return;
   }
 
   if (!state.bTimer) {
     // активация секундомера
-    state.bTimer = true
-    data.timerStart = performance.now()
+    state.bTimer = true;
+    data.timerStart = performance.now();
 
     /* присвоение и обновление реактивной переменной
     отражающей истёкшее время */
     data.stopwatch = setInterval(() => {
-      data.elapsedTime = performance.now() - data.timerStart
-      data.elapsedTimeStr = msToMinutes(data.elapsedTime)
+      data.elapsedTime = performance.now() - data.timerStart;
+      data.elapsedTimeStr = msToMinutes(data.elapsedTime);
       let charPerMin = Math.floor(
         (data.numDialed * 60) / (Math.floor(data.elapsedTime) / 1000)
       )
         .toString()
-        .padStart(3, '0')
-      if (charPerMin > 999) charPerMin = 999
-      data.charPerMin = charPerMin
-    }, 10)
+        .padStart(3, "0");
+      if (charPerMin > 999) charPerMin = 999;
+      data.charPerMin = charPerMin;
+    }, 10);
   }
 
-  recordingStatistics(e)
+  recordingStatistics(e);
 
-  if (code === 'Backspace' && data.indexArr > 0) {
+  if (code === "Backspace" && data.indexArr > 0) {
     /* возвращает предыдущий набор символов при условии нажатия
     клавиши backspace в нулевом положении курсора */
     if (data.indexArr % 200 === 0 && data.firstIndex !== 0) {
-      data.firstIndex -= 200
+      data.firstIndex -= 200;
     }
 
-    data.remainingChars++
-    moveCaret('back')
-    if ('23'.includes(data.statArr[data.indexArr]) && data.tempErrorFree > 0) {
-      data.tempErrorFree--
+    data.remainingChars++;
+    moveCaret("back");
+    if ("23".includes(data.statArr[data.indexArr]) && data.tempErrorFree > 0) {
+      data.tempErrorFree--;
     }
   } else {
-    if (storage.main.speaker) playAudio(click, storage.main.volume)
-    data.remainingChars--
-    moveCaret()
+    if (storage.main.speaker) playAudio(click, storage.main.volume);
+    data.remainingChars--;
+    moveCaret();
   }
 
   // срабатывает после того как был набран последний символ фрагмента
   if (
     data.indexArr >= data.fragmentArr.length ||
-    data.fragmentArr[data.indexArr] === 'end'
+    data.fragmentArr[data.indexArr] === "end"
   ) {
-    router.push({ name: 'stats' })
+    router.push({ name: "stats" });
   }
-}
+};
 
 onMounted(() => {
-  document.body.addEventListener('keydown', eListener)
-})
+  document.body.addEventListener("keydown", eListener);
+});
 
 onUnmounted(() => {
-  document.body.removeEventListener('keydown', eListener)
-  clearInterval(data.stopwatch)
-  data.charPerMin = 0
-})
+  document.body.removeEventListener("keydown", eListener);
+  clearInterval(data.stopwatch);
+  data.charPerMin = 0;
+});
 </script>
 
 <template>
@@ -173,24 +168,25 @@ onUnmounted(() => {
             char_display_correct:
               statArr[index + firstIndex] === '2' &&
               char !== ' ' &&
-              index < indexArr % 200
+              index < indexArr % 200,
           },
           {
             char_display_revised:
-              statArr[index + firstIndex] === '3' && index < indexArr % 200
+              statArr[index + firstIndex] === '3' && index < indexArr % 200,
           },
           {
             char_display_wrong:
-              statArr[index + firstIndex] === '4' && index < indexArr % 200
+              statArr[index + firstIndex] === '4' && index < indexArr % 200,
           },
           { 'char_display_special-inactive': charTest(char) },
           {
             'char_display_special-active':
               char !== 'skip' &&
               statArr[index + firstIndex] === '0' &&
-              index < indexArr % 200
-          }
-        ]">
+              index < indexArr % 200,
+          },
+        ]"
+      >
         <div v-if="char === 'skip'">&nbsp;</div>
         <div v-else-if="char === 'end'">&nbsp;</div>
         <div v-else>{{ char }}</div>
@@ -203,7 +199,8 @@ onUnmounted(() => {
       v-if="storage.visibility.keyboard"
       :event-keydown="events.keyDn"
       :char="data.fragmentArr[data.indexArr]"
-      :lang="data.keyboardLayout" />
+      :lang="data.keyboardLayout"
+    />
   </Transition>
 </template>
 
@@ -222,7 +219,7 @@ onUnmounted(() => {
   float: left;
   width: 36px;
   height: 76px;
-  font-family: 'Consolas', monospace;
+  font-family: "Consolas", monospace;
   font-size: 65px;
   color: v-bind(charColor);
   background: v-bind(charBackground);
@@ -236,7 +233,7 @@ onUnmounted(() => {
     position: absolute;
     right: 0;
     bottom: 0;
-    content: '';
+    content: "";
     width: 36px;
     height: 5px;
     background: v-bind(caretBackground);
